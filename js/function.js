375a901a47a7fe545618646d0d8498f8a8140bab
Evgeniya-SS <54635160+Evgeniya-SS@users.noreply.github.com>
2019-10-03 22:31:12 +0300

    js-f-0

--- js/function.js
+++ js/function.js
@@ -1,29 +1,28 @@
 function renderRow(target, data) {
     let HTML = '';
-    let quantity = 0;
+    let income = 0;
+    let expense = 0;
+    let balance = 0;
+    
 
     for (i=0; i<data.length; i++) {
         const obj = data[i];
+        
+        if (!obj.income || !obj.expense){
+            obj.income = 0;
+            obj.expense = 0;
+        }
 
-        if (!obj.month) {
-            continue;
-        }
-        if (!obj.income ||
-            !obj.expense) {
             
-            }
-        if (quantity === 12) {
-            break;
-        }
         if (obj.month) {
             HTML += `<div class="table-row">
                         <div class="cell">${obj.month}</div>
                         <div class="cell">Sausis</div>
                         <div class="cell">${obj.income} Eur</div>
                         <div class="cell">${obj.expense} Eur</div>
-                        <div class="cell">${obj.income + obj.expense} Eur</div>
+                        <div class="cell">${obj.income - obj.expense} Eur</div>
                     </div>`
-            quantity++;
+            
         }
         
     }
